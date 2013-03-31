e973d8ca6fa4cc822eed8330ef904bdf8a6ec75d
Mathieu Lecarme <mlecarme@bearstech.com>
2013-03-31 20:22:22 +0200

    Distutil stuffs.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='MySlow',
+      version='0.0.1',
+      description='Play with mysql slow log',
+      author='Mathieu Lecarme',
+      author_email='mlecarme@bearstech.com',
+      url='https://github.com/bearstech/myslow',
+      packages=['lib/myslow'],
+      )
