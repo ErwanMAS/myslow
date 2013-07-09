1a39b0e2a92e604f8eab14338dd98435d77dd77f
Mathieu Lecarme <mlecarme@bearstech.com>
2013-07-09 15:33:45 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='MySlow',
-      version='0.0.1',
+      version='0.0.2',
       description='Play with mysql slow log',
       author='Mathieu Lecarme',
       author_email='mlecarme@bearstech.com',
